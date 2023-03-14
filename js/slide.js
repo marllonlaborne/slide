75ec9279ccfb499b2632e76a3ac7d79abe7e26e1
Marllon <99225101+marllonlaborne@users.noreply.github.com>
2023-03-14 12:07:30 -0300

    configurações e index dos slides

--- js/slide.js
+++ js/slide.js
@@ -20,12 +20,11 @@ export default class Slide {
     if (event.type === 'mousedown') {
       event.preventDefault();
       this.dist.startX = event.clientX;
-      movetype = 'mousedown';
+      movetype = 'mousemove';
     } else {
       this.dist.startX = event.changedTouches[0].clientX;
       movetype = 'touchmove';
     }
-
     this.wrapper.addEventListener(movetype, this.onMove);
   }
 
@@ -36,7 +35,7 @@ export default class Slide {
   }
 
   onEnd(event) {
-    const movetype =(event.type === 'mouseup') ? 'mousemove' : 'touchmove';
+    const movetype = (event.type === 'mouseup') ? 'mousemove' : 'touchmove';
     this.wrapper.removeEventListener(movetype, this.onMove);
     this.dist.finalPosition = this.dist.movePosition;
   }
@@ -54,9 +53,39 @@ export default class Slide {
     this.onEnd = this.onEnd.bind(this);
   }
 
+  // Slides-config
+  slidePosition(slide) {
+    const margin = (this.wrapper.offsetWidth - slide.offsetWidth) / 2;
+    return -(slide.offsetLeft - margin);
+  }
+
+  slidesConfig() {
+    this.slideArray = [...this.slide.children].map((element) => {
+      const position = this.slidePosition(element);
+      return { position, element };
+    });
+  }
+
+  slideIndexNav(index) {
+    const last = this.slideArray.length - 1;
+    this.index = {
+      prev: index ? index - 1 : undefined,
+      current: index,
+      next: index === last ? undefined : index + 1,
+    }
+  }
+  
+  changeSlide(index) {
+    const activeSlide = this.slideArray[index];
+    this.moveSlide(activeSlide.position);
+    this.slideIndexNav(index);
+    this.dist.finalPosition = activeSlide.position;
+  }
+
   init() {
     this.bindEvents();
     this.addSlideEvents();
+    this.slidesConfig();
     return this;
   }
 }
